340b56340972dee95e95a0b49df1416e56e39590
1121608@isep.ipp.pt <>
2014-06-05 18:46:19 +0100

    (REPETIÇÃO) depois de resolvido o conflito no bitbucket
    Comentários na classe CP,CPController e CPUI.
    Criação de classe de teste (CPTest) para testar método alterado na CP.

--- src/controller/CriarCPController.java
+++ src/controller/CriarCPController.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import eventoscientificos.CP;
 import eventoscientificos.Empresa;
 import eventoscientificos.Evento;
@@ -10,65 +9,108 @@ import eventoscientificos.Utilizador;
 import java.util.List;
 
 /**
+ * Esta classe serve como controladora do fluxo entre a interface gráfica e a as
+ * classes intervenientes para a criação de uma CP(Comissão de Programa).
  *
- * @author Nuno Silva
+ * @author Grupo 66 LPR2
  */
+public class CriarCPController {
 
-public class CriarCPController
-{
+    /**
+     * Atributos de intância.
+     */
     private Empresa m_empresa;
     private Evento m_evento;
     private CP m_cp;
 
-    public CriarCPController(Empresa empresa)
-    {
-        m_empresa = empresa;
+    /**
+     * Construtor responsável por criar instância de empresa.
+     *
+     * @param empresa Empresa a ser intâncida
+     */
+    public CriarCPController(Empresa empresa) {
+        this.m_empresa = empresa;
     }
 
-    public List<Evento> getEventosOrganizador(String strId)
-    {
-        return m_empresa.getM_registoEventos().getEventosOrganizador(strId);
+    /**
+     * Procuara os eventos do organizador em função do seu id.
+     *
+     * @param strId endereço de email ou username do organizador
+     * @return lista de eventos do organizador
+     */
+    public List<Evento> getEventosOrganizador(String strId) {
+        return this.m_empresa.getM_registoEventos().getEventosOrganizador(strId);
     }
-    
-    public List<Topico> getTopicosEvento()
-    {
-        if( m_evento != null )
-            return m_evento.getTopicos();
-        else
+
+    /**
+     * Procura os tópicos do evento instânciado.
+     *
+     * @return lista de tópicos do evento
+     */
+    public List<Topico> getTopicosEvento() {
+        if (this.m_evento != null) {
+            return this.m_evento.getTopicos();
+        } else {
             return null;
+        }
     }
-    
-    public void selectEvento(Evento e)
-    {
-        m_evento = e;
-        
-        m_cp = m_evento.novaCP();
+
+    /**
+     * Seleciona o evento passado por parâmetro e cria um CP (Comissão de
+     * Programa) nesse mesmo evento.
+     *
+     * @param e evento a ser selecionado
+     */
+    public void selectEvento(Evento e) {
+        this.m_evento = e;
+
+        this.m_cp = this.m_evento.novaCP();
     }
-    
-    public Revisor addMembroCP(String strId)
-    {
-        Utilizador u = m_empresa.getM_registaUtilizador().getUtilizador(strId);
-        
-        if( u!=null)
-            return m_cp.addMembroCP( strId, u );
-        else
+
+    /**
+     * Adiciona membro Revisor a CP (Comissão de Programa) em função do seu id.
+     *
+     * @param strId endereço de email ou username do organizador
+     * @return objeto do tipo revisor
+     */
+    public Revisor addMembroCP(String strId) {
+        Utilizador u = this.m_empresa.getM_registaUtilizador().getUtilizador(strId);
+
+        if (u != null) {
+            return this.m_cp.addMembroCP(strId, u);
+        } else {
             return null;
+        }
     }
-    
-    public boolean registaMembroCP( Revisor r )
-    {
-        return m_cp.registaMembroCP(r);
+
+    /**
+     * Regista na lista de membros da CP (Comissão de Programa) o Revisor
+     * passado por parâmetro.
+     *
+     * @param r objeto do tipo Revisor
+     * @return verdadeiro/falso em função do registo verificado
+     */
+    public boolean registaMembroCP(Revisor r) {
+        return this.m_cp.registaMembroCP(r);
     }
-    
-    public void setCP()
-    {
-        m_evento.setCP(m_cp); 
+
+    /**
+     * Atribui a CP (Comissão de Programa) ao evento.
+     */
+    public void setCP() {
+        this.m_evento.setCP(this.m_cp);
     }
-    
-    public void setListaTopicosRevisor(Revisor r, List<Topico> listaTopicosRevisor)
-    {
-        if( m_cp.getListaRevisores().contains(r) )
-            r.setListaTopicos( listaTopicosRevisor );
+
+    /**
+     * Atribui lista de tópicos de perícia a Revisor da CP (Comissão de
+     * Programa).
+     *
+     * @param r objeto revisor da CP (Comissão de Programa)
+     * @param listaTopicosRevisor lista de tópicos de perícia
+     */
+    public void setListaTopicosRevisor(Revisor r, List<Topico> listaTopicosRevisor) {
+        if (this.m_cp.getListaRevisores().contains(r)) {
+            r.setListaTopicos(listaTopicosRevisor);
+        }
     }
 }
-
--- src/eventoscientificos/CP.java
+++ src/eventoscientificos/CP.java
@@ -1,57 +1,97 @@
 package eventoscientificos;
 
-
 import java.util.*;
 
 /**
+ * Classe responsável pela criação e gestão da CP (Comissão de Programa) criando
+ * para isso Revisores e respetiva lista.
  *
- * @author Nuno Silva
+ * @author Grupo 66 LPR2
  */
+public class CP {
 
-public class CP
-{
+    /**
+     * Atributos de intância.
+     */
     List<Revisor> m_listaRevisores;
 
-    public CP()
-    {
-        m_listaRevisores = new ArrayList();
+    /**
+     * Construtor responsável por instânciar lista de Revisores da CP (Comissão
+     * de Programa).
+     */
+    public CP() {
+        this.m_listaRevisores = new ArrayList();
     }
 
-    public Revisor addMembroCP( String strId, Utilizador u )
-    {
+    /**
+     * Cria um objeto do tipo Revisor membro da CP (Comissão de Programa).
+     *
+     * @param strId endereço de email ou username do revisor
+     * @param u objeto do tipo utilizador a ser associado a Revisor
+     * @return objeto do tipo Revisor se passar as validações e null se não
+     * passar
+     */
+    public Revisor addMembroCP(String strId, Utilizador u) {
         Revisor r = new Revisor(u);
-        
-        if( r.valida() && validaMembroCP(r) )
+
+        if (r.valida() && validaMembroCP(r)) {
             return r;
-        else
+        } else {
             return null;
+        }
     }
-    
-    private boolean validaMembroCP(Revisor r)
-    {
-        return true;
+
+    /**
+     * Verifica se o revisor já existe na CP (Comissão de Programa)
+     *
+     * @param r objeto do tipo Revisor
+     * @return verdadeiro se o Revisor já existe na CP e falso se ele já não
+     * existe na CP (Comissão de Programa)
+     */
+    public boolean validaMembroCP(Revisor r) {
+        for (Revisor rev : this.m_listaRevisores) {
+            if (rev.getUtilizador().getUsername().equalsIgnoreCase(r.getUtilizador().getUsername())
+                    && rev.getUtilizador().getEmail().equalsIgnoreCase(r.getUtilizador().getEmail())) {
+                return true;
+            }
+        }
+        return false;
     }
-    
-    public boolean registaMembroCP(Revisor r)
-    {
-        return m_listaRevisores.add(r);
+
+    /**
+     * Regista revisor na CP (Comissão de Programa).
+     *
+     * @param r objeto do tipo Revisor
+     * @return verdadeiro/falso em função do seu registo
+     */
+    public boolean registaMembroCP(Revisor r) {
+        return this.m_listaRevisores.add(r);
     }
-    
+
+    /**
+     * Cria um objeto do tipo String contendo toda a informação dos revisores
+     * presentes na CP (Comissão de programa).
+     *
+     * @return String com toda a informação da lista de revisores
+     */
     @Override
-    public String toString()
-    {
+    public String toString() {
         String strCP = "Membros de CP: ";
-        for( ListIterator<Revisor> it = m_listaRevisores.listIterator(); it.hasNext(); )
-        {
+        for (ListIterator<Revisor> it = this.m_listaRevisores.listIterator(); it.hasNext();) {
             strCP += it.next().toString();
-            if( it.hasNext() )
+            if (it.hasNext()) {
                 strCP += ", ";
+            }
         }
         return strCP;
     }
 
-    public List<Revisor> getListaRevisores() 
-    {
+    /**
+     * Procura a lista de revisores instânciada
+     *
+     * @return lista de revisores
+     */
+    public List<Revisor> getListaRevisores() {
         return this.m_listaRevisores;
     }
 }
