63a90b250b2b08e853c3baafa1a29b2cc58597d4
1121608@isep.ipp.pt <>
2014-06-17 02:21:25 +0100

    Acréscimo de alguns get's precisos para o UC9

--- src/eventoscientificos/Topico.java
+++ src/eventoscientificos/Topico.java
@@ -9,30 +9,42 @@ package eventoscientificos;
  * @author iazevedo
  */
 public class Topico {
-    
+
     private String m_strDescricao;
-    
+
     private String m_strCodigoACM;
 
-   
+    public Topico(String m_strDescricao, String m_strCodigoACM) {
+        this.m_strDescricao = m_strDescricao;
+        this.m_strCodigoACM = m_strCodigoACM;
+    }
+
+    Topico() {
+
+    }
+
     public void setDescricao(String strDescricao) {
         this.m_strDescricao = strDescricao;
     }
 
-
-
     public void setCodigoACM(String codigoACM) {
         this.m_strCodigoACM = codigoACM;
     }
-    
-    public boolean valida()
-    {
+
+    public boolean valida() {
         return true;
     }
-    
+
     @Override
-    public String toString()
-    {
-        return "\nCodigo: " + this.m_strCodigoACM +"\nDescrição: " + this.m_strDescricao;
+    public String toString() {
+        return "\nCodigo: " + this.m_strCodigoACM + "\nDescrição: " + this.m_strDescricao;
+    }
+
+    public String getM_strDescricao() {
+        return m_strDescricao;
+    }
+
+    public String getM_strCodigoACM() {
+        return m_strCodigoACM;
     }
 }
