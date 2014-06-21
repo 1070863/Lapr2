751ea27495bfd0076cf3f8ceb65cd9b098da9094
Pedro <Pedro@Pedro-Notebook>
2014-06-21 03:14:08 +0100

    Alteração do artigo.

--- test/eventoscientificos/ArtigoTest.java
+++ test/eventoscientificos/ArtigoTest.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package eventoscientificos;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -19,165 +19,110 @@ import static org.junit.Assert.*;
  * @author amartins
  */
 public class ArtigoTest {
-    
+
+    private List<Topico> topicos;
+
     public ArtigoTest() {
+        topicos = new ArrayList<>();
+        Topico topico1 = new Topico("A.1", "Teste");
+        Topico topico2 = new Topico("A.2", "Teste");
+        topicos.add(topico1);
+        topicos.add(topico2);
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
+
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     /**
-     * Test of setTitulo method, of class Artigo.
+     * Test of getConfianca method, of class Revisor.
      */
     @Test
-    public void testSetTitulo() {
-        System.out.println("setTitulo");
-        String strTitulo = "As 1001 vantagens de fazer testes";
+    public void testGetConfianca() {
+        System.out.println("getConfianca");
         Artigo instance = new Artigo();
-        instance.setTitulo(strTitulo);
-        assertEquals(strTitulo, instance.getTitulo());
-    }
-
-    /**
-     * Test of setResumo method, of class Artigo.
-     */
-    @Test
-    public void testSetResumo() {
-        System.out.println("setResumo");
-        String strResumo = "";
-        Artigo instance = new Artigo();
-        instance.setResumo(strResumo);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of novoAutor method, of class Artigo.
-     */
-    @Test
-    public void testNovoAutor_String_String() {
-        System.out.println("novoAutor");
-        String strNome = "";
-        String strAfiliacao = "";
-        Artigo instance = new Artigo();
-        Autor expResult = null;
-        Autor result = instance.novoAutor(strNome, strAfiliacao);
+        String expResult = "0";
+        instance.setConfianca(expResult);
+        String result = instance.getConfianca();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
-     * Test of novoAutor method, of class Artigo.
+     * Test of getAdequacao method, of class Revisor.
      */
     @Test
-    public void testNovoAutor_4args() {
-        System.out.println("novoAutor");
-        String strNome = "";
-        String strAfiliacao = "";
-        String strEmail = "";
-        Utilizador utilizador = null;
+    public void testGetAdequacao() {
+        System.out.println("getAdequacao");
         Artigo instance = new Artigo();
-        Autor expResult = null;
-        Autor result = instance.novoAutor(strNome, strAfiliacao, strEmail, utilizador);
+        String expResult = "1";
+        instance.setAdequacao(expResult);
+        String result = instance.getAdequacao();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
-     * Test of addAutor method, of class Artigo.
+     * Test of getOriginalidade method, of class Revisor.
      */
     @Test
-    public void testAddAutor() {
-        System.out.println("addAutor");
-        Autor autor = null;
+    public void testGetOriginalidade() {
+        System.out.println("getOriginalidade");
         Artigo instance = new Artigo();
-        boolean expResult = false;
-        boolean result = instance.addAutor(autor);
+        String expResult = "2";
+        instance.setOriginalidade(expResult);
+        String result = instance.getOriginalidade();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
-     * Test of getPossiveisAutoresCorrespondentes method, of class Artigo.
+     * Test of getQualidade method, of class Revisor.
      */
     @Test
-    public void testGetPossiveisAutoresCorrespondentes() {
-        System.out.println("getPossiveisAutoresCorrespondentes");
+    public void testGetQualidade() {
+        System.out.println("getQualidade");
         Artigo instance = new Artigo();
-        List<Autor> expResult = null;
-        List<Autor> result = instance.getPossiveisAutoresCorrespondentes();
+        String expResult = "3";
+        instance.setQualidade(expResult);
+        String result = instance.getQualidade();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
-     * Test of setAutorCorrespondente method, of class Artigo.
+     * Test of getRecomendacao method, of class Revisor.
      */
     @Test
-    public void testSetAutorCorrespondente() {
-        System.out.println("setAutorCorrespondente");
-        Autor autor = null;
+    public void testGetRecomendacao() {
+        System.out.println("getRecomendacao");
         Artigo instance = new Artigo();
-        instance.setAutorCorrespondente(autor);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setFicheiro method, of class Artigo.
-     */
-    @Test
-    public void testSetFicheiro() {
-        System.out.println("setFicheiro");
-        String strFicheiro = "";
-        Artigo instance = new Artigo();
-        instance.setFicheiro(strFicheiro);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setListaTopicos method, of class Artigo.
-     */
-    @Test
-    public void testSetListaTopicos() {
-        System.out.println("setListaTopicos");
-        List<Topico> listaTopicos = null;
-        Artigo instance = new Artigo();
-        instance.setListaTopicos(listaTopicos);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getInfo method, of class Artigo.
-     */
-    @Test
-    public void testGetInfo() {
-        System.out.println("getInfo");
-        Artigo instance = new Artigo();
-        String expResult = "";
-        String result = instance.getInfo();
+        boolean expResult = true;
+        instance.setRecomendacao(expResult);
+        boolean result = instance.getRecomendacao();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTextoJustificativo method, of class Revisor.
+     */
+    @Test
+    public void testGetTextoJustificativo() {
+        System.out.println("getTextoJustificativo");
+        Artigo instance = new Artigo();
+        String expResult = "Teste texto";
+        instance.setTextoJustificativo(expResult);
+        String result = instance.getTextoJustificativo();
+        assertEquals(expResult, result);
     }
 
     /**
@@ -187,11 +132,9 @@ public class ArtigoTest {
     public void testValida() {
         System.out.println("valida");
         Artigo instance = new Artigo();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.valida();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -219,32 +162,19 @@ public class ArtigoTest {
         myUt.setM_strEmail("expert@isep.ipp.pt");
         Autor act = new Autor();
         act.setUtilizador(myUt);
-        
+
         Artigo obj = new Artigo();
         obj.setTitulo("A beleza do JUnit");
         obj.setAutorCorrespondente(act);
         Artigo instance = new Artigo();
         instance.setTitulo("A beleza do JUnit");
         instance.setAutorCorrespondente(act);
-        
+
         boolean expResult = true;
         boolean result = instance.equals(obj);
         assertEquals(expResult, result);
     }
 
-    /**
-     * Test of hashCode method, of class Artigo.
-     */
-    @Test
-    public void testHashCode() {
-        System.out.println("hashCode");
-        Artigo instance = new Artigo();
-        int expResult = 0;
-        int result = instance.hashCode();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of getTitulo method, of class Artigo.
@@ -267,11 +197,68 @@ public class ArtigoTest {
     public void testGetResumo() {
         System.out.println("getResumo");
         Artigo instance = new Artigo();
-        String expResult = "";
+        String expResult = "Teste resumo";
+        instance.setTextoJustificativo(expResult);        
         String result = instance.getResumo();
+        assertEquals(expResult, result);
+    }
+
+
+    /**
+     * Test of setTipo method, of class Artigo.
+     * Testa o set do tipo de artigo.
+     */
+    @Test
+    public void testSetTipo() {
+        System.out.println("setTipo");
+        String tipo = "Full paper";
+        Artigo instance = new Artigo();
+        instance.setTipo(tipo);
+        String expResult = "Full paper";
+        String result = instance.getTipo();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getAutor method, of class Artigo.
+     */
+    @Test
+    public void testGetAutor() {
+        System.out.println("getAutor");
+        String id = "";
+        Artigo instance = new Artigo();
+        Autor expResult = null;
+        Autor result = instance.getAutor(id);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
+    /**
+     * Test of getM_strFicheiro method, of class Artigo.
+     */
+    @Test
+    public void testGetM_strFicheiro() {
+        System.out.println("getM_strFicheiro");
+        Artigo instance = new Artigo();
+        String expResult = "";
+        String result = instance.getM_strFicheiro();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getM_listaTopicos method, of class Artigo.
+     */
+    @Test
+    public void testGetM_listaTopicos() {
+        System.out.println("getM_listaTopicos");
+        Artigo instance = new Artigo();
+        instance.setListaTopicos(topicos);
+        List<Topico> expResult = topicos;
+        List<Topico> result = instance.getM_listaTopicos();
+        assertEquals(expResult, result);
+    }
+
 }
